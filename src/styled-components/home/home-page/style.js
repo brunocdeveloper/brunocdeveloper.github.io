0d34ffe5b0fb0e0e7d5b442713440f5370311b86
Bruno <brunocmoraes97@gmail.com>
2022-02-12 00:38:02 -0300

    Cria styled components para a home page e body

---
+++ src/styled-components/home/home-page/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const PresentationContainer = styled.div`
+
+
+`;
+
+const PresentationText = styled.div`
+
+`;
+
+const PresentationImage = styled.img`
+
+`;
+
+export { 
+  PresentationContainer,
+  PresentationText,
+  PresentationImage,
+}
