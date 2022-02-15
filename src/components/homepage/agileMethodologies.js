495cdc08d08b99ccd4ce636bca019db0e38bf779
Bruno <brunocmoraes97@gmail.com>
2022-02-15 14:51:30 -0300

    Implemente componente das metodologias ageis. Homepage finalizada

--- src/components/homepage/agileMethodologies.js
+++ src/components/homepage/agileMethodologies.js
@@ -1,9 +1,21 @@
-import react from 'react';
+import React from 'react';
+import { StyleGreenWithText } from '../../styled-components/home/home-page/style';
+import { SoftSkillsTex } from '../../styled-components/home/soft-skills-section/style';
+import { TitleSection } from '../../styled-components/home/technology-section/style';
 
 function AgileMethods() { 
   return ( 
     <div>
-
+      <TitleSection weight="700" size="52px">Metodologias Ágeis</TitleSection>
+      <SoftSkillsTex>
+        As metodologias ágeis, como o
+        <StyleGreenWithText> scrum</StyleGreenWithText> e <StyleGreenWithText>Kanban</StyleGreenWithText>,
+        são uma forma de acelerar, de forma organizada,
+        as entregas de um determinado projeto.
+        Esse método visa estimular uma gestão de processos que garanta
+        o controle e possíveis ajustes frequentemente, aumentando assim a
+        sinergia entre os trabalhos em grupo, o cliente e o projeto.
+      </SoftSkillsTex>
     </div>
   )
 }
