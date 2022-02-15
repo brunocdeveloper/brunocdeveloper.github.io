0b266aeb9b8d076ebcc80986fd4ea002c53f0d47
Bruno <brunocmoraes97@gmail.com>
2022-02-15 12:48:31 -0300

    Adiciona componentes com seçao de tecnologia

--- src/components/homepage/homePage.js
+++ src/components/homepage/homePage.js
@@ -1,11 +1,13 @@
 import ContainerHomePage from "./ContainerHomePage";
 import { BodyPage, GlobalStyle } from "../../styled-components/home/body/style";
+import Technologies from "./technologies";
 
 function HomePage() {
   return (
    <BodyPage>
       <GlobalStyle />
       <ContainerHomePage/>
+      <Technologies />
     </BodyPage> 
   )
 }
