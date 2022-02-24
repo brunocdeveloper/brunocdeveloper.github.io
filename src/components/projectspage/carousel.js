d1543a1fef18e3fdbb619c663849b7b109953249
Bruno <brunocmoraes97@gmail.com>
2022-02-24 17:25:35 -0300

    Refatora os botoes do carrossel e implementa funcionalidade de desativar o botao ao chegar no max range ou min range

--- src/components/projectspage/carousel.js
+++ src/components/projectspage/carousel.js
@@ -7,28 +7,67 @@ import rightBTN from '../projectspage/images/rightBTN.png';
 
 function Carousel() {
   const [dataProjects, setDataProjects] = useState(data);
+  const [opacityLastBtn, setOpacityLastBtn] = useState('0.5');
+  const [opacityNextBtn, setOpacityNextBtn] = useState();
+  const [eventRightBtn, setEventRightBtn] = useState();
+  const [eventLeftBtn, setEventLeftBtn] = useState('none');
+  const [countPositions, setCountPositions] = useState(1);
   const refCarousel = useRef(null);
+  let [initialPosition, setInitialPosition] = useState(1272);
 
   useEffect(() => {
     setDataProjects(data);
-  }, [])
+  }, []);
+
 
   function nextBtn() {
-    refCarousel.current.scrollLeft += refCarousel.current.offsetWidth
-  }
+    const dataLength = dataProjects.length - 1;
+    const maxLength = (dataLength * 1272) - 1272;
+
+    if(maxLength <= refCarousel.current.scrollLeft) {
+      setOpacityNextBtn('0.5');
+      setEventRightBtn('none');
+    }
+
+    if(countPositions > 0){
+      setEventLeftBtn('true');
+      setOpacityLastBtn('1');
+    }
+
+    refCarousel.current.scrollLeft += 1272;
+  };
 
   function lastBtn() {
-    refCarousel.current.scrollLeft -= refCarousel.current.offsetWidth
-  }
+    const dataLength = dataProjects.length - 1;
+    const maxLength = (dataLength * 1272) - 1272;
+
+    if(maxLength < refCarousel.current.scrollLeft) {
+      setOpacityNextBtn('1');
+      setEventRightBtn('true');
+    }
+
+    if (refCarousel.current.scrollLeft <= 1272) {
+      setEventLeftBtn('none');
+      setOpacityLastBtn('0.5');
+    }
+
+    refCarousel.current.scrollLeft -= 1272;
+  };
 
   return (
     <CardsAnButtonsCarousel>
-      <LeftButton onClick={() => lastBtn()}>
-        <img src={leftBTN} alt="image indicating left"/>
+      <LeftButton
+        onClick={() => {
+          lastBtn();
+        }}
+        opacity={opacityLastBtn}
+        events={eventLeftBtn}
+      >
+        <img src={leftBTN} alt="indicating left"/>
       </LeftButton>
       <CardCarousel ref={refCarousel}>
-        { dataProjects.map((data) => (
-          <ContainerCompleteCarousel>
+        { dataProjects.map((data, index) => (
+          <ContainerCompleteCarousel key={index}>
             <ContainerCarousel>
               <ImgProject src={data.projectIMG} alt={data.alt}/>
               <ContainerInfoProjects>
@@ -53,8 +92,14 @@ function Carousel() {
           </ContainerCompleteCarousel>
         ))}
       </CardCarousel>
-      <RightButton onClick={() => nextBtn()}>
-        <img src={rightBTN} alt="image indicating right"/>
+      <RightButton
+        onClick={() => { 
+          nextBtn();
+        }}
+        opacity={opacityNextBtn}
+        events={eventRightBtn}
+      >
+        <img src={rightBTN} alt="indicating right"/>
       </RightButton>
     </CardsAnButtonsCarousel>
   )
